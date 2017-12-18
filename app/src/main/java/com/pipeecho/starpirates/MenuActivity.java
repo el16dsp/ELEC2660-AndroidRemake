c5a3ec3bbd7a54928b0e902466e7b4be1b86db54
dspiper <dspiper@live.co.uk>
2017-12-18 19:55:16 +0000

    Got strings into the main menu's list

--- app/src/main/java/com/pipeecho/starpirates/MenuActivity.java
+++ app/src/main/java/com/pipeecho/starpirates/MenuActivity.java
@@ -2,6 +2,8 @@ package com.pipeecho.starpirates;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        // Make array of strings
+        String[] MenuItems = {"Play as Cutter", "Inspect Pistol", "Inspect Blowtorch"};
+        // Get list view id
+        ListView listView = (ListView) findViewById(R.id.MenuList);
+        // Build adapter for the list view
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, MenuItems);
+        // Set adapter to listview
+        listView.setAdapter(adapter);
     }
 }
