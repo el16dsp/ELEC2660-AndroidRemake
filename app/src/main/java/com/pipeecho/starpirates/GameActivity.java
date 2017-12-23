6d4ae074ed73e9ce55937cef35c7a915836f66ee
dspiper <dspiper@live.co.uk>
2017-12-23 14:23:33 +0000

    Added button.ong and developed some of the game activity

--- app/src/main/java/com/pipeecho/starpirates/GameActivity.java
+++ app/src/main/java/com/pipeecho/starpirates/GameActivity.java
@@ -3,6 +3,7 @@ package com.pipeecho.starpirates;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class GameActivity extends AppCompatActivity {
     int ClassSelected;
@@ -19,5 +20,17 @@ public class GameActivity extends AppCompatActivity {
         ClassSelected = intent.getIntExtra(MenuActivity.CHAR_SELECTED, 0);
 
         Game = new GameController(ClassSelected);
+
+        // TODO Change button text
+    }
+
+    public void onButton1Click(View view) {
+        // When button one pressed
+        // TODO Connect view to game
+    }
+
+    public void onButton2Click(View view) {
+        // When button two pressed
+        // TODO Connect view to game
     }
 }
